f576c89c75c10d069ca2462048c9e71520e7722b
xiongzhengjian <1913887509@qq.com>
2021-04-02 15:24:36 +0800

    write excel demo

--- demo1/Program.cs
+++ demo1/Program.cs
@@ -7,11 +7,16 @@ namespace demo1
     {
         static void Main(string[] args)
         {
-            
+            writeexcel();   
         }
 
-        public void exceldemo1() {
-            XLWorkbook workbook = new XLWorkbook();
+        public static  void writeexcel() {
+            var workbook = new XLWorkbook();
+            var worksheet = workbook.Worksheets.Add("Demo Sheet");
+            worksheet.Cell("A1").Value = "Hello ClosedXML";
+            worksheet.Cell("A2").FormulaA1 = "=MID(A1,7,5)";
+            // workbook.SaveAs(".\\output\\DemowriteExcel.xlsx");
+            workbook.SaveAs("DemowriteExcel.xlsx");
         }
     }
 }
